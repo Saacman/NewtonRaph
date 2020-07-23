270364dafc0d3a489f5b3f4a611ac2f5dbbee624
IsaacSF <saacman27@gmail.com>
2020-07-23 00:03:10 -0500

    Adding the velocity functions

--- 4-bar.py
+++ 4-bar.py
@@ -19,7 +19,7 @@ theta3_4 = np.array([[(20 * pi) / 180], [(30* pi) / 180]])
 # Calculate the Jacobian of the system
 # Measure the angle from the positive x-axis
 def createJacobian(t_3, t_4):
-    J = np.matrix([[ -a3 * sin(t_3), a4 * sin(t_4)],
+    J = np.array([[ -a3 * sin(t_3), a4 * sin(t_4)],
                     [ a3 * cos(t_3), -a4 * cos(t_4)]])
     return J
 
@@ -27,25 +27,43 @@ def createJacobian(t_3, t_4):
 def posFunc(t_3, t_4):
     f1 = a2 * cos(theta2) + a3 * cos(t_3) -  a1 * cos(theta1) - a4 * cos(t_4)
     f2 = a2 * sin(theta2) + a3 * sin(t_3) -  a1 * sin(theta1) - a4 * sin(t_4)
-    return np.matrix([[f1], [f2]])
+    return np.array([[f1], [f2]])
 
 
-def updateValues(thetas):
+def updatePosValues(thetas):
     f = posFunc(thetas[0][0], thetas[1][0])
     print(f"Position Functions:\nf_theta3: {f[0]}\nf_theta4: {f[1]}")
     J = createJacobian(thetas[0][0], thetas[1][0])
     print(f"Jacobian:\n{J}")
-    newF = thetas + np.linalg.inv(J) * (-f)
+    newF = thetas + np.dot(np.linalg.inv(J), (-f))
+    print(f"Inverse Jacobian:\n{np.linalg.inv(J)}")
     return newF
 
+# Evaluate the velocity functions, given the values
+# TODO Fix theta3 , theta4
+def velFunc(w_3, w_4):
+    f1 = -a2 * sin(theta2) * w2 - a3 * sin(theta3) * w_3 + a4 * sin(theta4) * w_4
+    f2 = a2 * cos(theta2) * w2 + a3 * cos(theta3) * w_3 - a4 * cos(theta4) * w_4
+    return np.array([[f1], [f2]])
+
+#TODO: FIX omegas3_4!!
+def updateVelValues(omegas):
+    f = velFunc(omegas[0][0], omegas[1][0])
+    print(f"Velocity Functions:\nf_theta3: {f[0]}\nf_theta4: {f[1]}")
+    J = createJacobian(theta3, theta4)
+    print(f"Jacobian:\n{J}")
+    newF = omegas + np.dot(np.linalg.inv(J), (-f))
+    print(f"Inverse Jacobian:\n{np.linalg.inv(J)}")
+    return newF
 
 if __name__ == '__main__':
-    
+
+    # Solve the Position of the system
     for i in range(3):
         print(f"Iteration {i}\n")
         old = theta3_4
-        theta3_4 = updateValues(theta3_4)
-        print(f" Theta_3_4:\n{(theta3_4 * 180)/ pi}")
+        theta3_4 = updatePosValues(theta3_4)
+        print(f" Theta_3_4:\n{(theta3_4 * 180)/pi}")
         print("")
         if abs(old[0][0]-theta3_4[0][0]) < 0.00001:
             break
