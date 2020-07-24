b508693090097c40bcaebf6e2471baac8da8b9c7
IsaacSF <saacman27@gmail.com>
2020-07-24 00:36:23 -0500

    Just before the test

--- 4-bar.py
+++ 4-bar.py
@@ -12,23 +12,13 @@ a3 = 12.3693
 a4 = 24.0416
 theta1 = 0
 theta2 = (60 * pi) / 180
+w2 = 20 # Known value from the beginning
+alpha2 = 0
 
 # Set initial position Values
 theta3_4 = np.array([[(10 * pi) / 180], [(130* pi) / 180]])
-
-# Velocity parameters of the mechanism
-theta3 = 0
-theta4 = 0
-w2 = 20 # Known value from the beginning
-
 #Set initial velocity values (rad/s)
 omegas3_4 = np.array([[0], [0]])
-
-# Acceleration parameters of the mechanism
-alpha2 = 0
-w3 = 0
-w4 = 0
-
 # Set initial acceleration values (rad/s2)
 alphas3_4 = np.array([[0], [0]])
 
@@ -58,14 +48,14 @@ def updatePosValues(thetas):
 
 # Evaluate the velocity functions, given the values
 def velFunc(w_3, w_4):
-    f1 = -a2 * sin(theta2) * w2 - a3 * sin(theta3) * w_3 + a4 * sin(theta4) * w_4
-    f2 = a2 * cos(theta2) * w2 + a3 * cos(theta3) * w_3 - a4 * cos(theta4) * w_4
+    f1 = -a2 * sin(theta2) * w2 - a3 * sin(theta3_4[0][0]) * w_3 + a4 * sin(theta3_4[1][0]) * w_4
+    f2 = a2 * cos(theta2) * w2 + a3 * cos(theta3_4[0][0]) * w_3 - a4 * cos(theta3_4[1][0]) * w_4
     return np.array([[f1], [f2]])
 
 def updateVelValues(omegas):
     f = velFunc(omegas[0][0], omegas[1][0])
     print(f"Velocity Functions:\nf_w3: {f[0]}\nf_w4: {f[1]}")
-    J = createJacobian(theta3, theta4)
+    J = createJacobian(theta3_4[0][0], theta3_4[1][0])
     print(f"Jacobian:\n{J}")
     newF = omegas + np.dot(np.linalg.inv(J), (-f))
     print(f"Inverse Jacobian:\n{np.linalg.inv(J)}")
@@ -73,14 +63,14 @@ def updateVelValues(omegas):
 
 # Evaluate the Acceleration functions, given the values
 def accFunc(alpha_3, alpha_4):
-    f1 = -a2 * sin(theta2) * alpha2 - a2 * cos(theta2) * pow(w2, 2) - a3 * sin(theta3) * alpha_3 - a3 * cos(theta3) * pow(w3, 2) + a4 * sin(theta4) * alpha_4 + a4 * cos(theta4) * pow(w4, 2)
-    f2 = a2 * cos(theta2) * alpha2 - a2 * sin(theta2) * pow(w2, 2) + a3 * cos(theta3) * alpha_3 - a3 * sin(theta3) * pow(w3, 2) - a4 * cos(theta4) * alpha_4 + a4 * sin(theta4) * pow(w4, 2)
+    f1 = -a2*sin(theta2)*alpha2 -a2*cos(theta2)*pow(w2, 2) -a3*sin(theta3_4[0][0])*alpha_3 -a3*cos(theta3_4[0][0])*pow(omegas3_4[0][0], 2) +a4*sin(theta3_4[1][0])*alpha_4 +a4*cos(theta3_4[1][0])*pow(omegas3_4[1][0], 2)
+    f2 = a2*cos(theta2)*alpha2 -a2*sin(theta2)*pow(w2, 2) +a3*cos(theta3_4[0][0])*alpha_3 -a3*sin(theta3_4[0][0])*pow(omegas3_4[0][0], 2) -a4*cos(theta3_4[1][0])*alpha_4 +a4*sin(theta3_4[1][0])*pow(omegas3_4[1][0], 2)
     return np.array([[f1], [f2]])
 
 def updateAccValues(alphas):
     f = accFunc(alphas[0][0], alphas[1][0])
     print(f"Acceleration Functions:\nf_alpha3: {f[0]}\nf_alpha4: {f[1]}")
-    J = createJacobian(theta3, theta4)
+    J = createJacobian(theta3_4[0][0], theta3_4[1][0])
     print(f"Jacobian:\n{J}")
     newF = alphas + np.dot(np.linalg.inv(J), (-f))
     print(f"Inverse Jacobian:\n{np.linalg.inv(J)}")
@@ -89,6 +79,7 @@ def updateAccValues(alphas):
 if __name__ == '__main__':
 
     # Solve the Position of the system
+    print("POSITION")
     for i in range(5):
         print(f"Iteration {i}\n")
         old = theta3_4
@@ -98,10 +89,7 @@ if __name__ == '__main__':
         if abs(old[0][0]-theta3_4[0][0]) < 0.00001:
             break
     
-    # Set the obtained values to use them later
-    theta3 = theta3_4[0][0]
-    theta4 = theta3_4[1][0]
-    
+    print("VELOCITY")
     # Solve the velocities of the system
     for i in range(5):
         print(f"Iteration {i}\n")
@@ -112,10 +100,10 @@ if __name__ == '__main__':
         if abs(old[0][0]-omegas3_4[0][0]) < 0.00001:
             break
     
-    # Set the obtained values to use them later
-    w3 = omegas3_4[0][0]
-    w4 = omegas3_4[1][0]
-    
+    theta3_4 = np.array([[(14.0362 * pi) / 180], [(135* pi) / 180]])
+    #Set initial velocity values (rad/s)
+    omegas3_4 = np.array([[29.3333], [11.2941]])
+    print("ACCELERATION")
     # Solve the accelerations of the system
     for i in range(10):
         print(f"Iteration {i}\n")
