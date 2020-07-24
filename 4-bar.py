8635b598f88bb126770d8fd4a71aaa50db0d8ec8
IsaacSF <saacman27@gmail.com>
2020-07-23 21:03:56 -0500

    More messy code

--- 4-bar.py
+++ 4-bar.py
@@ -3,26 +3,35 @@
 ## Isaac Sanchez - UGTO
 
 import numpy as np
-from math import cos, sin, pi, sqrt
+from math import cos, sin, pi, sqrt, pow
 
 # Position Parameters of the mechanism
-a1 = 1
-a2 = 1
-a3 = 2
-a4 = sqrt(2)
+a1 = 37
+a2 = 16.1245
+a3 = 12.3693
+a4 = 24.0416
 theta1 = 0
-theta2 = (90 * pi) / 180
+theta2 = (60 * pi) / 180
 
-#Set initial position Values
-theta3_4 = np.array([[(20 * pi) / 180], [(30* pi) / 180]])
+# Set initial position Values
+theta3_4 = np.array([[(10 * pi) / 180], [(130* pi) / 180]])
 
 # Velocity parameters of the mechanism
 theta3 = 0
 theta4 = 0
-w2 = 0
+w2 = 20 # Known value from the beginning
 
 #Set initial velocity values (rad/s)
 omegas3_4 = np.array([[0], [0]])
+
+# Acceleration parameters of the mechanism
+alpha2 = 0
+w3 = 0
+w4 = 0
+
+# Set initial acceleration values (rad/s2)
+alphas3_4 = np.array([[0], [0]])
+
 
 # Calculate the Jacobian of the system
 # Measure the angle from the positive x-axis
@@ -55,17 +64,32 @@ def velFunc(w_3, w_4):
 
 def updateVelValues(omegas):
     f = velFunc(omegas[0][0], omegas[1][0])
-    print(f"Velocity Functions:\nf_theta3: {f[0]}\nf_theta4: {f[1]}")
+    print(f"Velocity Functions:\nf_w3: {f[0]}\nf_w4: {f[1]}")
     J = createJacobian(theta3, theta4)
     print(f"Jacobian:\n{J}")
     newF = omegas + np.dot(np.linalg.inv(J), (-f))
     print(f"Inverse Jacobian:\n{np.linalg.inv(J)}")
     return newF
 
+# Evaluate the Acceleration functions, given the values
+def accFunc(alpha_3, alpha_4):
+    f1 = -a2 * sin(theta2) * alpha2 - a2 * cos(theta2) * pow(w2, 2) - a3 * sin(theta3) * alpha_3 - a3 * cos(theta3) * pow(w3, 2) + a4 * sin(theta4) * alpha_4 + a4 * cos(theta4) * pow(w4, 2)
+    f2 = a2 * cos(theta2) * alpha2 - a2 * sin(theta2) * pow(w2, 2) + a3 * cos(theta3) * alpha_3 - a3 * sin(theta3) * pow(w3, 2) - a4 * cos(theta4) * alpha_4 + a4 * sin(theta4) * pow(w4, 2)
+    return np.array([[f1], [f2]])
+
+def updateAccValues(alphas):
+    f = accFunc(alphas[0][0], alphas[1][0])
+    print(f"Acceleration Functions:\nf_alpha3: {f[0]}\nf_alpha4: {f[1]}")
+    J = createJacobian(theta3, theta4)
+    print(f"Jacobian:\n{J}")
+    newF = alphas + np.dot(np.linalg.inv(J), (-f))
+    print(f"Inverse Jacobian:\n{np.linalg.inv(J)}")
+    return newF
+
 if __name__ == '__main__':
 
     # Solve the Position of the system
-    for i in range(2):
+    for i in range(5):
         print(f"Iteration {i}\n")
         old = theta3_4
         theta3_4 = updatePosValues(theta3_4)
@@ -79,11 +103,24 @@ if __name__ == '__main__':
     theta4 = theta3_4[1][0]
     
     # Solve the velocities of the system
-    for i in range(2):
+    for i in range(5):
         print(f"Iteration {i}\n")
         old = omegas3_4
-        theta3_4 = updateVelValues(omegas3_4)
-        print(f" Theta_3_4:\n{(theta3_4 * 180)/pi}")
+        omegas3_4 = updateVelValues(omegas3_4)
+        print(f" Omegas_3_4:\n{omegas3_4}")
         print("")
-        if abs(old[0][0]-theta3_4[0][0]) < 0.00001:
+        if abs(old[0][0]-omegas3_4[0][0]) < 0.00001:
             break
+    
+    # Set the obtained values to use them later
+    w3 = omegas3_4[0][0]
+    w4 = omegas3_4[1][0]
+    
+    # Solve the accelerations of the system
+    for i in range(10):
+        print(f"Iteration {i}\n")
+        old = alphas3_4
+        alphas3_4 = updateAccValues(alphas3_4)
+        print(f" Alphas_3_4:\n{alphas3_4}")
+        print("")
+       
--- slider-cranck.py
+++ slider-cranck.py
@@ -11,14 +11,18 @@ a2 = 3
 a3 = 8
 theta_e = 0
 theta_2 = (40 * pi) / 180
-
+w2 = 209.43951
+alpha_2 = 0
 #Set initial Values
 theta3_s = np.array([[(300 * pi) / 180], [5]])
+w3_s = np.array([[0], [0]])
+alpha3_s = np.array([[0], [0]])
+
 
 # Calculate the Jacobian of the system
 # Measure the angle from the positive x-axis
 def createJacobian(t_3):
-    J = np.matrix([[ -a3 * sin(t_3), -1],
+    J = np.array([[ -a3 * sin(t_3), -1],
                     [ a3 * cos(t_3),  0]])
     return J
 
@@ -26,7 +30,7 @@ def createJacobian(t_3):
 def posFunc(t_3, s):
     f1 = a2 * cos(theta_2) + a3 * cos(t_3) - s
     f2 = a2 * sin(theta_2) + a3 * sin(t_3) +e
-    return np.matrix([[f1], [f2]])
+    return np.array([[f1], [f2]])
 
 
 def updateValues(thetas):
@@ -34,9 +38,38 @@ def updateValues(thetas):
     print(f"Position Functions:\nf_theta3: {f[0]}\nf_theta4: {f[1]}")
     J = createJacobian(thetas[0][0])
     print(f"Jacobian:\n{J}")
-    newF = thetas + np.linalg.inv(J) * (-f)
+    newF = thetas + np.dot(np.linalg.inv(J), (-f))
     return newF
 
+# Evaluate the velocity functions, given the values
+def velFunc(w_3, s):
+    f1 = -a2 * sin(theta_2) * w2 - a3 * sin(theta3_s[0][0]) * w_3 - s
+    f2 = a2 * cos(theta_2) * w2 + a3 * cos(theta3_s[0][0]) * w_3
+    return np.array([[f1], [f2]])
+
+def updateVelValues(omegas):
+    f = velFunc(omegas[0][0], omegas[1][0])
+    print(f"Velocity Functions:\nf_w3: {f[0]}\nf_w4: {f[1]}")
+    J = createJacobian(theta3_s[0][0])
+    print(f"Jacobian:\n{J}")
+    newF = omegas + np.dot(np.linalg.inv(J), (-f))
+    print(f"Inverse Jacobian:\n{np.linalg.inv(J)}")
+    return newF
+
+# Evaluate the Acceleration functions, given the values
+def accFunc(alpha_3, s):
+    f1 = -a2 * sin(theta_2) * alpha_2 - a2 * cos(theta_2) * pow(w2, 2) -a3 * sin(theta3_s[0][0]) * alpha_3 -a3 * cos(theta3_s[0][0]) * pow(w3_s[0][0], 2) - s
+    f2 = a2 * cos(theta_2) * alpha_2 - a2 * sin(theta_2) * pow(w2, 2) +a3 * cos(theta3_s[0][0]) * alpha_3 -a3 * sin(theta3_s[0][0]) * pow(w3_s[0][0], 2)
+    return np.array([[f1], [f2]])
+
+def updateAccValues(alphas):
+    f = accFunc(alphas[0][0], alphas[1][0])
+    print(f"Acceleration Functions:\nf_alpha3: {f[0]}\nf_alpha4: {f[1]}")
+    J = createJacobian(theta3_s[0][0])
+    print(f"Jacobian:\n{J}")
+    newF = alphas + np.dot(np.linalg.inv(J), (-f))
+    print(f"Inverse Jacobian:\n{np.linalg.inv(J)}")
+    return newF
 
 if __name__ == '__main__':
     
@@ -48,3 +81,23 @@ if __name__ == '__main__':
         print("")
         if abs(old[0][0]-theta3_s[0][0]) < 0.00001:
             break
+
+
+    
+    # Solve the velocities of the system
+    for i in range(5):
+        print(f"Iteration {i}\n")
+        old = w3_s
+        w3_s = updateVelValues(w3_s)
+        print(f" w3_s:\n{w3_s}")
+        print("")
+    
+
+    # Solve the accelerations of the system
+    for i in range(10):
+        print(f"Iteration {i}\n")
+        old = alpha3_s
+        alpha3_s = updateAccValues(alpha3_s)
+        print(f" Alphas_3_4:\n{alpha3_s}") #rad/s2 // in/s2
+        print("")
+       
